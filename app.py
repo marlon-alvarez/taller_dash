e87b3c3573931631059a1cce10919a013fc5791e
marlon.alvarez <marlon.alvarez@itac.co>
2026-01-27 20:45:05 -0500

    commit para enviar al repositorio remoto en github

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import pandas as pd
 import datetime as dt
 
 # esto es un cambio de prueba para hacer commit sobre una nueva rama en git.
+# esto es un cambio de prueba para hacer commit sobre una nueva rama en git.
 
 app = dash.Dash(
     __name__,
